5b8a7f06e6fc57cb1b43db28678288929621ccce
samuelsenerwa <samuelsenerwa@gmail.com>
2024-05-29 19:51:49 +0300

    feat:creating new record

--- odoo-custom-addons/my_library/models/library_book_categ.py
+++ odoo-custom-addons/my_library/models/library_book_categ.py
@@ -23,6 +23,26 @@ class BookCategory(models.Model):
     _parent_name = "parent_id"  # optional if field is 'parent_id'
     parent_path = fields.Char(index=True)
 
+    # creating a new record
+    def create_categories(self):
+        categ1 = {
+            'name': 'Child Category 1',
+            'description': 'Description for child 1'
+        }
+        categ2 = {
+            'name': 'Child category 2',
+            'description': 'Description for child 2'
+        }
+        parent_category_val = {
+            'name': 'Parent category',
+            'email': 'Description for parent category',
+            'child_ids': [
+                (0, 0, categ1),
+                (0, 0, categ2),
+            ]
+        }
+        record = self.env['library.book.category'].create(parent_category_val)
+
     @api.constrains('parent_id')
     def _check_hierarchy(self):
         if not self.check_recursion():
