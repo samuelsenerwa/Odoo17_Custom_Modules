a09c6ed30d5659334e73ccac823f55009b872a5f
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-11 10:19:57 +0300

    chore:fixed name search function by commenting it out

--- odoo-custom-addons/my_library/models/library_book.py
+++ odoo-custom-addons/my_library/models/library_book.py
@@ -317,26 +317,26 @@ class ResPartner(models.Model):
         for r in self:
             r.count_books = len(r.authored_book_ids)
 
-    def name_get(self):
-        result = []
-        for book in self:
-            authors = book.author_ids.mapped('name')
-            name = '%s (%s)' % (book.name, ', '.join(authors))
-            result.append((book.id, name))
-        return result
-
-    @api.model
-    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
-        args = [] if args is None else args.copy()
-        if not (name == '' and operator == 'ilike'):
-            args += ['|', '|', '|',
-                     ('name', operator, name),
-                     ('isbn', operator, name),
-                     ('author_ids.name', operator, name)
-                     ]
-        return super(LibraryBook, self)._name_search(
-            name=name, args=args, operator=operator,
-            limit=limit, name_get_uid=name_get_uid)
+    # def name_get(self):
+    #     result = []
+    #     for book in self:
+    #         authors = book.author_ids.mapped('name')
+    #         name = '%s (%s)' % (book.name, ', '.join(authors))
+    #         result.append((book.id, name))
+    #     return result
+    #
+    # @api.model
+    # def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+    #     args = [] if args is None else args.copy()
+    #     if not (name == '' and operator == 'ilike'):
+    #         args += ['|', '|', '|',
+    #                  ('name', operator, name),
+    #                  ('isbn', operator, name),
+    #                  ('author_ids.name', operator, name)
+    #                  ]
+    #     return super(ResPartner, self)._name_search(
+    #         name=name, args=args, operator=operator,
+    #         limit=limit, name_get_uid=name_get_uid)
 
     # extracting grouped results using read_group()
     def grouped_data(self):
