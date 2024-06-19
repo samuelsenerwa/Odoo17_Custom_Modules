f5162a67d003689fe36f5b2129301a4deb51ea4a
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-19 17:02:24 +0300

    feat:chore refactored the file organization and content

--- odoo-custom-addons/pos_restriction/__manifest__.py
+++ odoo-custom-addons/pos_restriction/__manifest__.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 {
     'name': "POS Stock Restriction",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
-
+    'summary': 'Restricts selling out of stock product quantities in the POS',
+    'version': '17.0.1.1',
     'description': """
-Long description of module's purpose
+ This module adds a restriction to prevent selling products that are out of stock in the POS.
+        - Adds a checkbox to the POS interface to enable/disable the restriction.
+        - Allows selection of warehouse location for the POS session.
+        - Shows a warning when attempting to sell out of stock products.
     """,
 
-    'author': "My Company",
+    'author': "Samuel Senerwa",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Point of Sale',
+
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'point_of_sale', 'stock'],
 
     # always loaded
     'data': [
@@ -38,4 +40,5 @@ Long description of module's purpose
     ],
     'application': False,
     'installable': True,
+    'license': 'LGPL-3',
 }
