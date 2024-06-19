e279e1e3e06b10685ff02efd7df0f8bc62835133
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-19 18:15:05 +0300

    feat:defined class stock quant & added methods to get stock

--- odoo-custom-addons/pos_restriction/models/pos_config.py
+++ odoo-custom-addons/pos_restriction/models/pos_config.py
@@ -34,3 +34,64 @@ class ResConfigSettings(models.TransientModel):
         string='Stock Location',
         readonly=False
     )
+
+
+class pos_order(models.Model):
+    _inherit = "pos.order"
+
+    location_id = fields.Many2one(
+        comodel_name='stock.location',
+        related='config_id.stock_location_id',
+        string="Location", store=True,
+        readonly=True,
+    )
+
+
+# determine the stock from all the locations existing in the system
+class stock_quant(models.Model):
+    _inherit = 'stock.quant'
+
+    def get_stock_location_qty(self, location):
+        res = {}
+        product_ids = self.env['product.product'].search([])
+        for product in product_ids:
+            quants = self.env['stock.quant'].search(
+                [('product_id', '=', product.id), ('location_id', '=', location['id']),
+                 ('company_id', '=', self.env.user.company_id.id)])
+            if len(quants) > 1:
+                quantity = 0.0
+                for quant in quants:
+                    quantity += quant.quantity
+                res.update({product.id: quantity})
+            else:
+                res.update({product.id: quants.quantity})
+        return [res]
+
+    def get_products_stock_location_qty(self, location, products):
+        res = {}
+        product_ids = self.env['product.product'].browse(products)
+        for product in product_ids:
+            quants = self.env['stock.quant'].search(
+                [('product_id', '=', product.id), ('location_id', '=', location),
+                 ('company_id', '=', self.env.user.company_id.id)])
+            if len(quants) > 1:
+                quantity = 0.0
+                for quant in quants:
+                    quantity += quant.quantity
+                res.update({product.id: quantity})
+            else:
+                res.update({product.id: quants.quantity})
+        return [res]
+
+    def get_single_product(self, product, location):
+        res = []
+        pro = self.env['product.product'].browse(product)
+        quants = self.env['stock.quant'].search([('product_id', '=', pro.id), ('location', '=', location)])
+        if len(quants) > 1:
+            quantity = 0.0
+            for quant in quants:
+                quantity += quant.quantity
+            res.append([pro.id, quantity])
+        else:
+            res.append([pro.id, quants.quantity])
+        return res
