6a70e8b7eca1d79450279e048aab406195438d44
samuelsenerwa <samuelsenerwa@gmail.com>
2024-06-26 16:27:21 +0300

    feat: made RPC call to odoo backend

--- odoo-custom-addons/pos_demo/static/src/js/pos_demo.js
+++ odoo-custom-addons/pos_demo/static/src/js/pos_demo.js
@@ -21,5 +21,41 @@ odoo.define('pos_demo.custom', function (require) {
         },
     });
     Registries.Component.add(PosDiscountButton);
+
+//    Making RPC call
+    class PosLastOrderButton extends PosComponent {
+        async onClick() {
+            var self = this;
+            const order = this.env.pos.get_order();
+            if (order.attributes.client) {
+                var domain = [['partner_id', '=', order.attributes.client.id]];
+                this.rpc({
+                    model: 'pos.order',
+                    method:'search_read',
+                    args:[domain, ['name', 'amount_total']],
+                    kwargs: {limit:5},
+                }).then(function(orders){
+                    if(orders.length > 0) {
+                    var order_list = _.str.sprint("%s - TOTAL: %s", o.name, o.amount_total)
+                    };
+                });
+            } else {
+                self,.showPopup('ErrorPopup', {
+                    title: self.env._t('Error'),
+                    body: self.env._t("Please select a customer."),
+                });
+            }
+        }
+    }
+    PosLastOrderButton.template = 'PosLastOrderButton';
+    ProductScreen.addControlButton({
+        component: PosLastOrderButton,
+        condition: function() {
+        return true;
+//            return this.env.pos.config.module_pos_last_order;
+        },
+    });
+    Registries.Component.add(PosLastOrderButton);
+
     return PosDiscountButton;
 });
