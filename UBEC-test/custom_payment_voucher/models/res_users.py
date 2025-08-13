0df88c50025c604c43b294d1ac419e36bfc47984
Samuel Senerwa <66512735+samuelsenerwa@users.noreply.github.com>
2025-08-13 10:18:32 +0300

    Delete UBEC-test directory

--- UBEC-test/custom_payment_voucher/models/res_users.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    user_signature = fields.Binary(string="User Signature")
--- UBEC-test/custom_payment_voucher/wizards/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import comments_wizard
-
-
